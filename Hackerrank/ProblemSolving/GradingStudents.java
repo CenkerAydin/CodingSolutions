2d50c699baec1dcfeda8e629bf75716c568f323e
Cenker Aydın <cenkeraydincse@gmail.com>
2023-06-30 09:41:09 +0300

    Create GradingStudents.java

---
+++ Hackerrank/ProblemSolving/GradingStudents.java
@@ -0,0 +1,15 @@
+package ProblemSolving;
+
+import java.util.List;
+
+public class GradingStudents {
+    public static List<Integer> gradingStudents(List<Integer> grades){
+        for (int i=0;i<grades.size();i++){
+            if (grades.get(i)<38){
+            }
+            if (grades.get(i)+1 %5==0 || grades.get(i)+2 %5==0){
+
+            }
+        }
+    }
+}
